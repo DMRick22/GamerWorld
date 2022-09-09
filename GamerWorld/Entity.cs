fc4c14b932c3edff296e0f401462726d9129842f
ricca <ricca@DESKTOP-ANIV812>
2022-09-09 15:04:32 +0200

    Add project files.

---
+++ GamerWorld/Entity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GamerWorld
+{
+    internal abstract class Entity
+    {
+        private int id;
+
+        public Entity()
+        {
+
+        }
+        protected Entity(int id)
+        {
+            Id = id;
+        }
+
+        public int Id { get => id; set => id = value; }
+
+        public override string ToString()
+        {
+            return $"ID: {id}\n";
+        }
+    }
+}
